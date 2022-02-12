9584365f11dd426533eca5648e69f42fe5be189c
AliyunESC <itsalbertzhang@gmail.com>
2022-02-12 17:54:45 +0800

    commit by aliyun ECS

--- day24/3.pthread_create/1.default.c
+++ day24/3.pthread_create/1.default.c
@@ -1,6 +1,6 @@
 #include <func.h>
 
-void *pthreadfunc(void *arg) {//注册一个子线程函数
+void *pthreadfunc(void *arg) { //注册一个子线程函数
     printf("Here is child thread.\n");
 }
 
@@ -9,7 +9,7 @@ int main(int argc, char const *argv[]) {
     pthread_t pthid;
     ret = pthread_create(&pthid, NULL, pthreadfunc, NULL);
     THREAD_ERR_CHECK(ret, "pthread_create");
-    usleep(1000);//睡一下, 否则主线程有可能在子线程之前关闭.
+    usleep(1000); //睡一下, 否则主线程有可能在子线程之前关闭.
     printf("Here is main thread.\n");
     return 0;
 }
